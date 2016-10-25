c7fc897f2a3128f2e10653e7d67b853b21a7b0a3
Conor Burke & Kelsey <pair+conorburke.kedskeds@devbootcamp.com>
2016-10-24 17:59:35 -0700

    Add test for bottle song lyrics

--- bottle_song_lyrics.rb
+++ bottle_song_lyrics.rb
@@ -2,5 +2,25 @@
 # Returns the lyrics to "n Bottles of Beer".
 
 def bottle_song_lyrics(n)
-  "The lyrics to #{n} Bottles of Beer"
+  str = ""
+  while n > 0 do
+    if n > 2
+      str += "#{n} bottles of beer on the wall, #{n} bottles of beer."
+      str += "\n"
+      str += "Take one down, pass it around, #{n-1} bottles of beer on the wall!"
+      str += "\n"
+    elsif n == 2
+      str += "#{n} bottles of beer on the wall, #{n} bottles of beer."
+      str += "\n"
+      str += "Take one down, pass it around, #{n-1} bottle of beer on the wall!"
+      str += "\n"
+    else
+      str += "#{n} bottle of beer on the wall, #{n} bottle of beer."
+      str += "\n"
+      str += "Take one down, pass it around, no more bottles of beer on the wall!"
+      str += "\n"
+    end
+    n -= 1
+  end
+  str
 end
--- spec/bottle_song_spec.rb
+++ spec/bottle_song_spec.rb
@@ -1,5 +1,7 @@
 require_relative '../bottle_song_lyrics'
 
 describe 'bottle_song_lyrics' do
-  # Your tests here ...
+  it 'prints bottle song lyrics' do
+    expect(bottle_song_lyrics(2)). to eq "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down, pass it around, 1 bottle of beer on the wall!\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down, pass it around, no more bottles of beer on the wall!\n"
+  end
 end
