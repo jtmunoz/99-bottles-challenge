03cebcef93c121c68630348899b2f4b42cd4fba9
Tae Yun <yuntjs@gmail.com>
2016-10-25 23:03:51 -0700

    complete 99 bottles song without loop

--- bottle_song_lyrics.rb
+++ bottle_song_lyrics.rb
@@ -2,5 +2,14 @@
 # Returns the lyrics to "n Bottles of Beer".
 
 def bottle_song_lyrics(n)
-  "The lyrics to #{n} Bottles of Beer"
+    return puts "1 bottle of beer on the wall, 1 bottle of beer\nTake one down, pass it around, no bottles of beer on the wall!" if n == 1
+    puts "#{n} bottles of beer on the wall, #{n} bottles of beer"
+    if n-1 > 1
+      puts "Take one down, pass it around, #{n-1} bottles of beer on the wall!"
+    else
+      puts "Take one down, pass it around, 1 bottle of beer on the wall!"
+    end
+    bottle_song_lyrics(n-1)
 end
+
+bottle_song_lyrics(99)
