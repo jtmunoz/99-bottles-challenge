632a15c2aeaf90b6a44c7a80b52b0d1836cfc95d
cheleylin <linqiaoshu1992@gmail.com>
2016-10-26 17:55:19 -0700

    implement method to create bottle_lyrics

--- bottle_song_lyrics.rb
+++ bottle_song_lyrics.rb
@@ -2,5 +2,17 @@
 # Returns the lyrics to "n Bottles of Beer".
 
 def bottle_song_lyrics(n)
-  "The lyrics to #{n} Bottles of Beer"
+
+  return "" if n == 0
+
+  if n == 2
+    "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down, pass it around, 1 bottle of beer on the wall!
+    \n" + bottle_song_lyrics(n-1)
+  elsif n == 1
+    "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down, pass it around, no more bottles of beer on the wall!
+   \n" + bottle_song_lyrics(n-1)
+  else
+    "#{n} bottles of beer on the wall, #{n} bottles of beer.\nTake one down, pass it around, #{n-1} bottles of beer on the wall!
+    \n" + bottle_song_lyrics(n-1)
+  end
 end
