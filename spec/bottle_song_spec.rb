4374c4170725af5cedbf06851f8fad1c8a7127be
Jordan Munoz <email": null>
2016-10-29 13:14:04 -0700

    adds testing for a String class

--- spec/bottle_song_spec.rb
+++ spec/bottle_song_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../bottle_song_lyrics'
 
 describe 'bottle_song_lyrics' do
-  # Your tests here ...
+
+ it 'returns a string' do
+  expect(bottle_song_lyrics(20).class).to eq String
+end
+
+
+
 end
