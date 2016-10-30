1584b58549aa6b2c8ef0bd18e69e052ef71b30d9
conorburke <cjburke89@gmail.com>
2016-10-30 16:04:58 -0700

    Complete with recursion

--- bottle_song_lyrics.rb
+++ bottle_song_lyrics.rb
@@ -2,5 +2,17 @@
 # Returns the lyrics to "n Bottles of Beer".
 
 def bottle_song_lyrics(n)
-  "The lyrics to #{n} Bottles of Beer"
+  if n == 1 
+    "1 bottle of beer on the wall, 1 bottle of beer. 
+    Take one down, pass it around, no more bottles of beer on the wall!"
+  
+  elsif n == 2 
+    "2 bottles of beer on the wall, 2 bottles of beer.  
+    Take one down, pass it around, 1 bottle of beer on the wall!" + bottle_song_lyrics(n-1)
+  
+  else 
+    "#{n} bottles of beer on the wall, #{n} bottles of beer. 
+    Take one down, pass it around, #{n-1} bottles of beer on the wall!" + bottle_song_lyrics(n-1)
+  end
+
 end
