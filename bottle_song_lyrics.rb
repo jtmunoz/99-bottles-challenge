708bee89042d20d189ada9c81f55eca4d69cd1fd
keithrjames <jamekeit@isu.edu>
2016-10-30 15:21:59 -0700

    finish 99 bottles challenge.

--- bottle_song_lyrics.rb
+++ bottle_song_lyrics.rb
@@ -1,6 +1,28 @@
 # Takes as its input an integer +n+ representing the starting number of bottles.
 # Returns the lyrics to "n Bottles of Beer".
 
+
 def bottle_song_lyrics(n)
-  "The lyrics to #{n} Bottles of Beer"
+  p "#{n} bottles of beer on the wall, #{n} bottles of beer. Take one down, pass it around, #{n-1} bottles of beer on the wall!"
 end
+
+def no_more_bottles(n)
+    p "#{n} bottles of beer on the wall, #{n} bottles of beer. Take one down, pass it around, no bottles of beer on the wall!"
+end
+
+
+def nineninebottles(n)
+  until n == 0
+    if n == 1
+      no_more_bottles(n)
+    else
+      bottle_song_lyrics(n)
+    end
+    n -= 1
+  end
+end
+
+nineninebottles(10)
+
+
+
